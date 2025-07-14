57b97853f5fdd2480efb918780768448e1de06bc
jaindivyanshu1 <jaindivyanshu.personal@gmail.com>
2025-07-15 00:44:23 +0530

    Setup frontend with react-vite app and tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gray-100 text-gray-900">
+      <h1 className="text-3xl font-bold">Welcome to TravelEase 🌍</h1>
+    </div>
+  );
+}
+
+export default App;
+
